40c328b167d81e73a06b139c47f0a8ecd8683473
iamnague <nague_@hotmail.com>
2022-10-28 15:35:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,39 +2,41 @@ var cron = require('node-cron');
 const fs = require('fs');
 const date = require('date-and-time');
 
-console.log('accessToken: ' + process.env.accessToken);
-console.log('refreshToken: ' + process.env.refreshToken);
-console.log('usagePointId: ' + process.env.usagePointId);
+var accessToken = process.env.accessToken;
+var refreshToken =  process.env.refreshToken;
+var usagePointId = process.env.usagePointId;
 
+console.log('accessToken: ' + accessToken);
+console.log('refreshToken: ' + refreshToken);
+console.log('usagePointId: ' + usagePointId);
 
 function FetchEnedis(){
 
 const linky = require('linky');
 
-// Créez une session
+// Créer une session
 const session = new linky.Session({
+    accessToken,
+    refreshToken,
+    usagePointId,
+    onTokenRefresh: (accessToken, refreshToken) => {
 
-    accessToken: process.env.accessToken,
-    refreshToken:  process.env.refreshToken,
-    usagePointId: process.env.usagePointId,
-//    onTokenRefresh: (accessToken, refreshToken) => {
-
+         console.log('accessToken renew: ' + accessToken);
+         console.log('refreshToken renew: ' + refreshToken);
+    },
 });
 
-//cron.schedule('* * * * *', () => {
 var today = new Date();
 var yesterday = new Date();
 yesterday.setDate(yesterday.getDate()-arguments[0]);
 today = date.format(today, 'YYYY-MM-DD');
 yesterday  = date.format(yesterday, 'YYYY-MM-DD');
 
-// Récupère la puissance moyenne consommée le 31 décembre 2021, sur un intervalle de 30 min
 session.getLoadCurve(yesterday, today).then((result) => {
    fs.writeFile('linky_power.json', JSON.stringify(result, null, 2), function (err) {
       if (err) return console.log(err);
       console.log(new Date(), 'OK Power from ', yesterday);
       });
-
 });
 
 session.getDailyConsumption(yesterday, today).then((result) => {
@@ -46,12 +48,4 @@ session.getDailyConsumption(yesterday, today).then((result) => {
 }
 
 FetchEnedis(7); //immediate execution
-
-function scheduler (delay) {
-cron.schedule("23 0-23/3 * * *", () => FetchEnedis(delay),{
-   scheduled: true,
-   timezone: "Europe/Paris"
- }); //periodically execution.
-}
-
-scheduler (1);
+                                                      
